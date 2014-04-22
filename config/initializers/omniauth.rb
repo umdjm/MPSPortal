9543d8f8c0c6cc5bece87a7b0f687608d3d62446
DJ Martin <umdjm@hotmail.com>
2014-04-22 16:26:51 -0400

    v4

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,10 +1,5 @@
 require 'forcedotcom'
 
-# Set the default hostname for omniauth to send callbacks to.
-# seems to be a bug in omniauth that it drops the httpS
-# this still exists in 0.2.0
-OmniAuth.config.full_host = ENV['consumer-key']
-
 module OmniAuth
   module Strategies
     #tell omniauth to load our strategy
